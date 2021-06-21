0bc8ab1c836c8dd02fc4c8d8d50a6594de4393e6
michael Kaner <michael.kaner@towa.at>
2021-06-21 13:42:20 +0200

    changed snackbar layout

--- src/ServiceWorkerWrapper.tsx
+++ src/ServiceWorkerWrapper.tsx
@@ -1,11 +1,19 @@
 import React, { FC, useEffect } from 'react';
-import { Snackbar, Button } from '@material-ui/core';
+import { Snackbar, Button, makeStyles, Theme } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 import * as serviceWorker from './serviceWorkerRegistration';
 
+const useStyles = makeStyles((theme: Theme) => ({
+    button: {
+      marginLeft: theme.spacing(2)
+    },
+  }));
+  
 const ServiceWorkerWrapper: FC = () => {
     const [showReload, setShowReload] = React.useState(false);
     const [waitingWorker, setWaitingWorker] = React.useState<ServiceWorker | null>(null);
 
+    const classes = useStyles();
 
     const onSWUpdate = (registration: ServiceWorkerRegistration) => {
       setShowReload(true);
@@ -28,16 +36,19 @@ const ServiceWorkerWrapper: FC = () => {
         message="A new version is available!"
         onClick={reloadPage}
         anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
-        action={
-          <Button
+      >
+        <MuiAlert severity="info">
+                A new Version is available!
+                <Button
             color="inherit"
             size="small"
             onClick={reloadPage}
+            className={classes.button}
           >
             Reload
           </Button>
-        }
-      />
+        </MuiAlert>
+      </Snackbar>
     );
   }
   
