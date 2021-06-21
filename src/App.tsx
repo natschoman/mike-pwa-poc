8c5f9fc85242f91719b8afdb61fcb4c794b77460
Natschoman <natschoman@gmail.com>
2021-06-21 12:16:32 +0200

    add install button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+let deferredPrompt: any;
 
 function App() {
+  const [installable, setInstallable] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      setInstallable(true);
+    });
+
+    window.addEventListener("appinstalled", () => {
+      // Log install to analytics
+      console.log("INSTALL: Success");
+    });
+  }, []);
+
+  const handleInstallClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    // Hide the app provided install promotion
+    setInstallable(false);
+    // Show the install prompt
+    deferredPrompt.prompt();
+    // Wait for the user to respond to the prompt
+    deferredPrompt.userChoice.then((choiceResult: any) => {
+      if (choiceResult.outcome === "accepted") {
+        console.log("User accepted the install prompt");
+      } else {
+        console.log("User dismissed the install prompt");
+      }
+    });
+  };
+
   return (
     <div className="App">
+      {installable && (
+        <button className="install-button" onClick={handleInstallClick}>
+          INSTALL ME
+        </button>
+      )}
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
